355d82aeea6fe64b56c625628d60b5418cf96303
beckyjobita <122250572+BeckyJobita@users.noreply.github.com>
2024-07-21 08:11:55 -0700

    Add files via upload

---
+++ TheLocalYield/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TheLocalYield project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheLocalYield.settings')
+
+application = get_wsgi_application()
